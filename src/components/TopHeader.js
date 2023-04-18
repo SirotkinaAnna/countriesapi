1e4b9cb5a1ef1b57f9f7c27dffe836a41c73a31f
SirotkinaAnna <saurann88@gmail.com>
2023-04-18 13:21:30 -0400

    dark mode toggle

--- src/components/TopHeader.js
+++ src/components/TopHeader.js
@@ -1,16 +1,18 @@
 import { BsMoon, BsFillMoonFill } from "react-icons/bs";
 import { useState } from "react";
 function TopHeader() {
-    const [mode, toggleMode] = useState(false)
+    const [mode, toggleMode] = useState(true);
+    const allDiv = document.querySelectorAll("*");
+
     const handleDarkMode = () => {
-        const allDiv = document.querySelectorAll("*");
+
         toggleMode(!mode);
         if (mode) {
             for (let i = 0; i < allDiv.length; i++) {
                 allDiv[i].classList.add('dark')
             }
         }
-        else {
+        else if (!mode) {
             for (let i = 0; i < allDiv.length; i++) {
                 allDiv[i].classList.remove('dark')
             }
@@ -19,7 +21,7 @@ function TopHeader() {
 
     return <nav className="bg-white flex justify-between px-4 md:px-20 py-6 drop-shadow-md">
         <h1 className="md:font-bold text-md md:text-lg">Where in the world?</h1>
-        <button className="flex justify-center items-center gap-2 text-sm" onClick={handleDarkMode}>{mode ? <BsMoon /> : <BsFillMoonFill />}Dark Mode</button>
+        <button className="flex justify-center items-center gap-2 text-sm" onClick={handleDarkMode}>{!mode ? <BsFillMoonFill /> : <BsMoon />}Dark Mode</button>
 
     </nav>
 }
