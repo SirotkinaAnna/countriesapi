a2ea81b3e98473f63262cfe33859fdac0648cd01
SirotkinaAnna <saurann88@gmail.com>
2023-04-18 13:01:02 -0400

    some css

--- src/components/TopHeader.js
+++ src/components/TopHeader.js
@@ -18,7 +18,7 @@ function TopHeader() {
     }
 
     return <nav className="bg-white flex justify-between px-4 md:px-20 py-6 drop-shadow-md">
-        <h1 className="font-bold text-lg">Where in the world?</h1>
+        <h1 className="md:font-bold text-md md:text-lg">Where in the world?</h1>
         <button className="flex justify-center items-center gap-2 text-sm" onClick={handleDarkMode}>{mode ? <BsMoon /> : <BsFillMoonFill />}Dark Mode</button>
 
     </nav>
