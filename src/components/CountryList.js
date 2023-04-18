20574bd32cd05a3355ebc046f657f1f311417a97
SirotkinaAnna <saurann88@gmail.com>
2023-04-18 16:01:12 -0400

    alt

--- src/components/CountryList.js
+++ src/components/CountryList.js
@@ -14,7 +14,7 @@ function CountryList({ item, allData }) {
         })
     }
     return <div className="h-full bg-white input-inside  text-sm rounded-lg shadow-md">
-        <div className="h-56 md:h-48 rounded-t-lg input-inside"><img className="h-full w-full rounded-t-lg " src={item.flags.png} alt={item.flags.alt} /></div>
+        <div className="h-56 md:h-48 rounded-t-lg input-inside"><img className="h-full w-full rounded-t-lg " src={item.flags.png} alt="flag" /></div>
         <div className="px-8  md:h-max div-dark-border div-dark py-6 md:py-4"> <h1 onClick={handleClick} className="cursor-pointer text-lg font-bold text-base py-4">{item.name}</h1>
             <p className="pb-2"><span className="font-semibold">Population: </span>{sortPopulation(item.population)}</p>
             <p className="pb-2"><span className="font-semibold ">Region: </span>{item.region}</p>
